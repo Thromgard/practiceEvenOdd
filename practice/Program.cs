0dfad5c380bcebb1adfc80a706a84caed66dc9b7
djhansen <david.j.hansen@gmail.com>
2020-06-17 14:35:03 -0400

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer : ");
+            var ans = Console.ReadLine();
+            var nbr = Convert.ToInt32(ans);
+            if (nbr % 2 == 0) {
+                Console.WriteLine($"The number {nbr} is even");
+            } else
+            {
+                Console.WriteLine($"The number {nbr} is odd");
+            }
+        }
+    }
+}
